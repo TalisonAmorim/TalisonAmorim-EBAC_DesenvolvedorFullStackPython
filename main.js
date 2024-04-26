aa8fa777baa2e7f803a02a10cf1f4d1d4c1629e0
Talison Amorim <amorimtalison9@gmail.com>
2024-04-26 16:06:03 -0300

    tarefas

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    alert('olá Mundo')
-})
+$(document).ready(function() {
+    $('#task-form').submit(function(e) {
+      e.preventDefault();
+      let taskName = $('#task-input').val();
+      var taskCount = $('#task-list li').length + 1; 
+    $('#task-list').append('<li>'+ taskCount + '. ' + taskName + '</li>');
+      $('#task-input').val('');
+    });
+  
+    $(document).on('click', '#task-list li', function() {
+      $(this).toggleClass('completed');
+    });
+  });
+  
