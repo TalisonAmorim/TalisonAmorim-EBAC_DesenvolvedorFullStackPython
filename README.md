b795379e2490858c3073c250780b10a5770a8123
Talison Amorim <amorimtalison9@gmail.com>
2024-04-29 19:10:04 -0300

    formulário com mascara

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
-# Lista de Tarefas com jQuery
+# Formulário com Mascaras jQuery
 
-Este é um projeto desenvolvido como parte do curso de [Desenvolvedor Full Stack Python da EBAC](https://ebaconline.com.br/full-stack-python). O objetivo é criar uma aplicação de lista de tarefas utilizando HTML, CSS, JavaScript e jQuery.
+Este é um projeto desenvolvido como parte do curso de [Desenvolvedor Full Stack Python da EBAC](https://ebaconline.com.br/full-stack-python). O objetivo é criar um formulário de cadastro utilizando HTML, CSS, JavaScript e jQuery, com os campos: nome completo, e-mail, telefone, CPF, endereço completo e CEP, utilize o plugin jQuery Mask Plugin para aplicar máscara aos campos CPF, telefone e CEP
 
 ## Requisitos
 
-O exercício consiste em:
+A entrega deste exercício consiste em:
 
-1. Criar uma aplicação de lista de tarefas contendo um formulário com um campo para adicionar o nome da tarefa e um botão para cadastrar a tarefa.
-2. Ao submeter o formulário, adicionar um elemento `<li>` dentro de uma lista ordenada ou não.
-3. Ao clicar nos itens da lista, adicionar um efeito que aplique uma linha em cima do texto (text-decoration: line-through).
-4. Utilizar o jQuery para manipular os elementos e controlar os eventos.
-5. Criar uma branch chamada `exercicio_jquery` no repositório do curso e enviar o link através da plataforma.
+1. Crie um formulário de cadastro, este formulário deverá conter os campos: nome completo, e-mail, telefone, CPF, endereço completo e CEP;
+2. Utilize o plugin jQuery Mask Plugin para aplicar máscara aos campos CPF, telefone e CEP;
+3. Crie uma branch chamada exercicio_plugins_jquery no repositório do curso;
+4. Envie o link através da plataforma.
 
 ## Estrutura
 
 - `index.html`: Contém a estrutura HTML da aplicação.
-- `style.css`: Arquivo de estilos CSS para estilizar a aplicação.
-- `script.js`: Arquivo JavaScript contendo a lógica do aplicativo, incluindo o uso do jQuery.
+- `main.css`: Arquivo de estilos CSS para estilizar a aplicação.
+- `main.js`: Arquivo JavaScript contendo a lógica do aplicativo, incluindo o uso do jQuery.
 - `README.md`: Documentação do projeto.
 - `imagens`: Pasta contendo as Imagens do projeto.
-
-
--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 $(document).ready(function() {
-  $('#task-form').submit(function(e) {
+  // Aplicando máscaras aos campos
+  $('#telefone').mask('(00) 0000-0000');
+  $('#cpf').mask('000.000.000-00', {reverse: true});
+  $('#cep').mask('00000-000');
+});
+
+$(document).ready(function() {
+  // Função para adicionar uma nova linha na tabela
+  $('#cadastroForm').submit(function(e) {
     e.preventDefault();
-    var taskName = $('#task-input').val();
-    var taskCount = $('#task-list li').length + 1; // Conta o número de tarefas e incrementa
-    $('#task-list').append('<li><input type="checkbox" class="task-checkbox">' + taskCount + '. ' + taskName + '<button class="delete-btn">&#128465;</button></li>');
-    $('#task-input').val('');
+
+    // Captura dos valores dos campos do formulário
+    var nomeCompleto = $('#nomeCompleto').val();
+    var email = $('#email').val();
+    var telefone = $('#telefone').val();
+    var cpf = $('#cpf').val();
+    var endereco = $('#endereco').val();
+    var cep = $('#cep').val();
+
+    // Adiciona os valores em uma nova linha na tabela
+    $('#dadosTabela').append('<tr>' +
+                             '<td>' + nomeCompleto + '</td>' +
+                             '<td>' + email + '</td>' +
+                             '<td>' + telefone + '</td>' +
+                             '<td>' + cpf + '</td>' +
+                             '<td>' + endereco + '</td>' +
+                             '<td>' + cep + '</td>' +
+                             '<td><button class="excluir-btn">&#128465;</button></td>' + // Botão de exclusão
+                             '</tr>');
+
+    // Limpa os campos do formulário após a submissão
+    $('#cadastroForm')[0].reset();
   });
 
-  $('#task-list').on('click', '.task-checkbox', function(e) {
-    e.stopPropagation(); 
-    var $taskItem = $(this).parent();
-    $taskItem.toggleClass('completed', $(this).prop('checked'));
+  // Função para excluir uma linha da tabela quando o botão "Excluir" é clicado
+  $('#dadosTabela').on('click', '.excluir-btn', function() {
+    $(this).closest('tr').remove();
   });
-
-  $('#task-list').on('click', '.delete-btn', function(e) {
-    e.stopPropagation(); 
-    $(this).parent().remove();
-  });
-  
 });
