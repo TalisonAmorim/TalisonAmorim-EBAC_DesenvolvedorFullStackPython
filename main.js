bbaece8e45feb1e6f3d1fa01f2d772dd6145a025
Talison Amorim <amorimtalison9@gmail.com>
2024-04-29 10:40:53 -0300

    ajustes propostos

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-    $('#task-form').submit(function(e) {
-      e.preventDefault();
-      let taskName = $('#task-input').val();
-      var taskCount = $('#task-list li').length + 1; 
-    $('#task-list').append('<li>'+ taskCount + '. ' + taskName + '</li>');
-      $('#task-input').val('');
-    });
-  
-    $(document).on('click', '#task-list li', function() {
-      $(this).toggleClass('completed');
-    });
+  $('#task-form').submit(function(e) {
+    e.preventDefault();
+    var taskName = $('#task-input').val();
+    var taskCount = $('#task-list li').length + 1; // Conta o número de tarefas e incrementa
+    $('#task-list').append('<li><input type="checkbox" class="task-checkbox">' + taskCount + '. ' + taskName + '<button class="delete-btn">&#128465;</button></li>');
+    $('#task-input').val('');
   });
-  
+// botão para excluir tarefa
+  $('#task-list').on('click', '.delete-btn', function() {
+    $(this).parent().remove();
+  });
+//ajuste proposto pelo professor
+  $('#task-list').on('click', 'li', function() {
+    $(this).toggleClass('completed');
+  });
+});
