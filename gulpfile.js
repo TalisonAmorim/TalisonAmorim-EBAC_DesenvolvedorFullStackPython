012e45e56f0d253f5f7e97264ec96a42dad00239
Talison Amorim <amorimtalison9@gmail.com>
2024-05-27 13:12:18 -0300

    compila sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps')
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss') // Define o arquivo de entrada
+        .pipe(sourcemaps.init()) // Inicializa a criação de sourcemaps
+        .pipe(sass({
+            outputStyle: 'compressed' // Configura o Sass para comprimir o CSS de saída
+        }))
+        .pipe(sourcemaps.write('./maps')) // Escreve os sourcemaps no diretório especificado
+        .pipe(gulp.dest('./build/styles')); // Define o diretório de saída dos arquivos CSS compilados
+}
 
 function funcaoPadrao(callback){
-    console.log("Executando via Gulp");
-    callback();
+    setTimeout(function(){
+        console.log("Executando via Gulp");
+        callback();
+
+    },2000);
 }
 
 function dizOi(callback) {
@@ -15,5 +30,10 @@ function dizTchau(){
     console.log("Tchau Gulp");
 }
 
-exports.default = gulp.series(funcaoPadrao,dizOi);
+// exports.default = gulp.series(funcaoPadrao,dizOi);
+exports.default = gulp.parallel(funcaoPadrao,dizOi);
 exports.dizOi = dizOi;
+exports.sass = compilaSass
+exports.watch = function(){
+    gulp.watch('./source/styles/*.scss',{ignoreInitial: false}, gulp.series(compilaSass));
+}
